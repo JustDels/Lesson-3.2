f4d9531736cb24adb8f9db1f88f02b9d382253a0
Dmitry Toporkov <dels.axe@gmail.com>
2021-08-16 20:06:33 +0500

    First commit

---
+++ src/main/java/ru/netology/domain/Post.java
@@ -0,0 +1,14 @@
+package ru.netology.domain;
+
+public class Post {
+    private int id;
+    private int authorId;
+    private int date;
+    private String content;
+    private int likeCounter;
+    private int commentCounter;
+    private int shareCounter;
+    private int viewCounter;
+
+    // + get/set на все поля
+}
